56d0231a288fcf71a68d72089fdf8da8f780420a
david.chien <f312213213david@gmail.com>
2023-11-27 02:02:04 +0800

    feat: connect sql

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
 from routers import users
+from sql import models
+from sql.database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
---
+++ sql/database.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+# pylint: disable-next=line-too-long
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE')}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()

87d7be9f943b039fe8d32b09378994bbbb8dee4c
david.chien <f312213213david@gmail.com>
2023-11-27 23:09:14 +0800

    feat: connect cloudflare r2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from routers import users
 from sql import models
 from sql.database import engine
+from static_file import r2
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,3 +11,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
+
+
+@app.get("/")
+def get_all_content_in_r2():
+    return r2.list_all_files()
---
+++ static_file/r2.py
@@ -0,0 +1,25 @@
+import os
+
+import boto3
+from botocore.config import Config
+from dotenv import load_dotenv
+
+s3_donfig = Config(retries={"max_attempts": 10, "mode": "standard"})
+
+load_dotenv()
+
+s3 = boto3.resource(
+    service_name="s3",
+    endpoint_url=os.getenv("R2_URL"),
+    aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("R2_ACCESS_TOKEN"),
+)
+
+r2 = s3.Bucket(os.getenv("R2_BUCKET_NAME"))
+
+
+def list_all_files():
+    file_list = []
+    for obj in r2.objects.all():
+        file_list.append(f"{os.getenv('R2_FILE_PATH')}/{obj.key}")
+    return file_list

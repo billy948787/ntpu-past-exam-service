e7dd9b8a2e526404438ef8e8a7fe6cd8104aa245
david.chien <f312213213david@gmail.com>
2023-12-18 19:58:32 +0800

    feat: send mail

--- departments/dependencies.py
+++ departments/dependencies.py
@@ -1,13 +1,19 @@
+import os
 from typing import Dict
 
+from dotenv import load_dotenv
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from bulletins.models import Bulletin
 from courses.models import Course
 from users.models import User, UserDepartment
+from utils.send_mail import send_notification_mail
 
 from . import models
+
+load_dotenv()
+domain = os.getenv("ORIGIN")
 
 
 def get_department_bulletins(db: Session, department_id: str):
@@ -82,12 +88,13 @@ def get_departments_status(db: Session, user_id: str):
 
 
 def request_view_department(db: Session, request: Dict):
-    if (
+    department = (
         db.query(models.Department)
         .filter(models.Department.id == request["department_id"])
         .first()
-        is None
-    ):
+    )
+
+    if department is None:
         raise HTTPException(status_code=404)
 
     past_request = (
@@ -107,6 +114,28 @@ def request_view_department(db: Session, request: Dict):
     db.add(db_request)
     db.commit()
     db.refresh(db_request)
+
+    admins = get_department_admins(db=db, department_id=department.id)
+
+    recipients = []
+
+    for admin in admins:
+        recipients.append(
+            admin.email
+            if admin.email is not None
+            else f"s{admin.username}@gm.ntpu.edu.tw"
+        )
+
+    send_notification_mail(
+        title="請求審核",
+        content=f"有人申請瀏覽您所管理的 {department.name} 社群，請儘速審核。",
+        recipients=recipients,
+        cta={
+            "text": "前往審核",
+            "link": f"{domain}/admin/{department.id}?open_edit_member_dialog=true",
+        },
+    )
+
     return db_request
 
 
@@ -117,6 +146,22 @@ def approve_request_view_department(db: Session, request_id: str):
 
     request.status = "APPROVED"
     db.commit()
+
+    department = (
+        db.query(models.Department)
+        .filter(models.Department.id == request.department_id)
+        .first()
+    )
+
+    user = db.query(User).filter(User.id == request.user_id).first()
+
+    send_notification_mail(
+        title="審核通過",
+        content=f"您申請加入 {department.name} 的審核已通過。",
+        recipients=[
+            user.email if user.email is not None else f"s{user.username}@gm.ntpu.edu.tw"
+        ],
+    )
 
 
 def update_member_admin(db: Session, department_id: str, user_id: str, is_admin: bool):
@@ -133,6 +178,35 @@ def update_member_admin(db: Session, department_id: str, user_id: str, is_admin:
 
     request.is_department_admin = is_admin
     db.commit()
+
+    department = (
+        db.query(models.Department)
+        .filter(models.Department.id == request.department_id)
+        .first()
+    )
+
+    user = db.query(User).filter(User.id == request.user_id).first()
+
+    if is_admin:
+        send_notification_mail(
+            title="權限變更",
+            content=f"您在 {department.name} 的權限已被提升至『管理員』。",
+            recipients=[
+                user.email
+                if user.email is not None
+                else f"s{user.username}@gm.ntpu.edu.tw"
+            ],
+        )
+    else:
+        send_notification_mail(
+            title="權限變更",
+            content=f"您在 {department.name} 的權限已被變更至『一般用戶』。",
+            recipients=[
+                user.email
+                if user.email is not None
+                else f"s{user.username}@gm.ntpu.edu.tw"
+            ],
+        )
 
 
 def get_department_information(db: Session, department_id: str):
@@ -175,6 +249,18 @@ def get_join_requests(db: Session, department_id: str):
         )
 
     return result
+
+
+def get_department_admins(db: Session, department_id: str):
+    sub_query = db.query(UserDepartment.department_id).filter(
+        # pylint: disable-next=singleton-comparison
+        (UserDepartment.is_department_admin == True)
+        & (UserDepartment.department_id == department_id)
+        & (UserDepartment.user_id == User.id)
+    )
+
+    admins = db.query(User).filter(sub_query.exists()).all()
+    return admins
 
 
 def get_department_members(db: Session, department_id: str):
--- posts/dependencies.py
+++ posts/dependencies.py
@@ -6,12 +6,16 @@ from dotenv import load_dotenv
 from sqlalchemy.orm import Session
 
 from courses.models import Course
+from departments.dependencies import get_department_admins, get_department_information
 from static_file.r2 import r2
 from users.models import User
+from utils.send_mail import send_notification_mail
 
 from . import models
 
 load_dotenv()
+
+domain = os.getenv("ORIGIN")
 
 
 def get_posts(
@@ -107,9 +111,47 @@ def make_post(db: Session, post: Dict, user_id: str, file_array: List[bytes]):
 
     db.commit()
     db.refresh(db_post)
+
+    department = get_department_information(db=db, department_id=post["department_id"])
+    admins = get_department_admins(db=db, department_id=post["department_id"])
+    recipients = []
+    for admin in admins:
+        recipients.append(
+            admin.email
+            if admin.email is not None
+            else f"s{admin.username}@gm.ntpu.edu.tw"
+        )
+    send_notification_mail(
+        title="考古題審核",
+        content=f"有人在您所管理的 {department.name} 社群發布考古題，請儘速審核。",
+        recipients=recipients,
+        cta={
+            "text": "前往審核",
+            "link": f"{domain}/admin/{department.id}?open_edit_post_dialog=true&edit_post_detail_id={db_post.id}",
+        },
+    )
     return db_post
 
 
 def update_post_status(db: Session, post_id: str, status: str):
-    (db.query(models.Post).filter(models.Post.id == post_id).update({"status": status}))
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    post.status = status
     db.commit()
+
+    user = db.query(User).filter(User.id == post.owner_id).first()
+    department = get_department_information(db=db, department_id=post.department_id)
+
+    if status == "APPROVED":
+        send_notification_mail(
+            title="考古題審核通過",
+            content=f"您在『{department.name}』發布標題為『{post.title}』的考古題已通過管理員的審核。",
+            recipients=[
+                user.email
+                if user.email is not None
+                else f"s{user.username}@gm.ntpu.edu.tw"
+            ],
+            cta={
+                "text": "查看考古題",
+                "link": f"{domain}/{department.id}/{post.course_id}/{post.id}",
+            },
+        )
